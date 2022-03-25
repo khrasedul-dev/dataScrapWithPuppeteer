d82795fd1fda137e8b2f26fc8171690383543e32
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-03-25 16:47:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.get("/", async (req, res) => {
     })
     
     const page = await browser.newPage()
+
+    page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36')
     
     page.setDefaultNavigationTimeout(100000)
 
